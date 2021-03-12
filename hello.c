15674a3f7a7fe43c7481ad7893c107a5a834fe42
Cameron Henlin <cam.henlin@gmail.com>
2021-03-11 23:45:05 -0800

    add untested read code - need to figure out devenv before testing further

--- hello.c
+++ hello.c
@@ -6,16 +6,18 @@
 
 char* application_id = "TEST_APPLICATION_1";
 int call_counter = 0;
-IOParam pb2; // TODO name this better, what does pb stand for?
+IOParam outgoingSerialPortReference; // TODO I think this is an accurate name
+IOParam incomingSerialPortReference;
 
 /*
-Read more: http://stason.org/TULARC/os-macintosh/programming/7-1-How-do-I-get-at-the-serial-ports-Communications-and-N.html#ixzz4cIxU3Tob
+// http://mirror.informatimago.com/next/developer.apple.com/documentation/mac/Devices/Devices-320.html
+Read more: http://stason.org/TULARC/os-macintosh/programming/7-1-How-do-I-get-at-the-serial-ports-Communications-and-N.html#ixzz4cIxU3Tob this one is only useful for enumerating ports
+// https://developer.apple.com/library/archive/documentation/mac/pdf/Devices/Serial_Driver.pdf
+Serial implementation:
 
-Serial implementation: 
-
-https://opensource.apple.com/source/gdb/gdb-186.1/src/gdb/ser-mac.c?txt 
+https://opensource.apple.com/source/gdb/gdb-186.1/src/gdb/ser-mac.c?txt another example of a serial library
 http://mirror.informatimago.com/next/developer.apple.com/documentation/mac/Devices/Devices-320.html
-*/ 
+*/
 
 // notes from above article:
 // You can use OpenDriver, SetReset, SetHShake, SetSetBuf, SerGetBuf and
@@ -24,171 +26,202 @@ http://mirror.informatimago.com/next/developer.apple.com/documentation/mac/Devic
 // To write to the
 // serial port, use FSWrite for synchronous writes that wait until all is
 // written, or PBWrite asynchronously for queuing up data that is supposed
-// to go out but you don't want to wait for it.  
+// to go out but you don't want to wait for it.
 
 // At least once each time
 // through your event loop, you should call SerGetBuf on the in driver
 // reference number you got from OpenDriver, and call FSRead for that many
 // bytes - neither more nor less.
 
+void setupPBControlForSerialPort(short serialPortShort) {
 
-void setupSerialPort(short refNum)
-{ 
-#define MODEM_PORT_OUT   "\p.AOut" 
-#define MODEM_PORT_IN    "\p.AIn" 
-#define PRINTER_PORT_OUT "\p.BOut" 
-#define PRINTER_PORT_IN  "\p.BIn"
-    
-    const char* nameStr = "";
-    switch (refNum)
-    {
-        case aoutRefNum:
-            nameStr = MODEM_PORT_OUT;
-            break;
-        case boutRefNum:
-            nameStr = PRINTER_PORT_OUT;
-            break;   
-
-        // input device not valid for writing data
-		// TODO, figure this out -- assuming we'll need these to be able to read back responses?
-        /*
-        case ainRefNum:
-            nameStr = MODEM_PORT_IN;
-            break;
-        case binRefNum:
-            nameStr = MODEM_PORT_IN;
-            break;         
-        */
-            
-        default:
-            return;        
-    }
-    
-    short serialPort = 0;
-    OSErr err = MacOpenDriver(nameStr, &serialPort);
-   
-    if (err < 0) {
-		
-		return;
-	}
-    
-	// TODO: cb seems sort of unused here - what is this for? figure this out
-	// TODO at least create a better name for it
     CntrlParam cb;
-    cb.ioCRefNum = serialPort;
-    cb.csCode = 8;
+    cb.ioCRefNum = serialPortShort; // TODO: this is always 0
+    cb.csCode = 8; // TODO: not sure what this is for
     cb.csParam[0] = stop10 | noParity | data8 | baud9600; // TODO: can we achieve higher than 9600 baud?
-    err = PBControl ((ParmBlkPtr) & cb, 0);
+    err = PBControl ((ParmBlkPtr) & cb, 0); // PBControl definition: http://mirror.informatimago.com/next/developer.apple.com/documentation/mac/Networking/Networking-296.html
+    // second param there is async -- could be OK for what we are doing? TODO: investigate
 
     if (err < 0) {
-		
-		return;
-	}
-            
-    pb2.ioRefNum = serialPort;
+
+        return;
+    }
+
+}
+
+// all of the "inbound" stuff here is TODO
+void setupSerialPort(const char *name) {
+#define MODEM_PORT_OUT   "\p.AOut"
+#define MODEM_PORT_IN    "\p.AIn"
+#define PRINTER_PORT_OUT "\p.BOut"
+#define PRINTER_PORT_IN  "\p.BIn"
+
+    const char* serialPortOutputName = "";
+    const char* serialPortInputName = "";
+
+    if (strcmp (name, "modem") == 0) {
+
+        serialPortOutputName = MODEM_PORT_OUT;
+        serialPortInputName = MODEM_PORT_IN;
+    } else if (strcmp (name, "printer") == 0) {
+
+        serialPortOutputName = PRINTER_PORT_OUT;
+        serialPortInputName = MODEM_PORT_IN;
+    } else {
+
+        return;
+    }
+
+    short serialPortOutput = 0; // TODO: why is this always 0? is this right?
+    short serialPortInput = 1; // TODO: not realy sure what this should be - just incrementing from the last item here
+
+    OSErr err = MacOpenDriver(serialPortOutputName, &serialPortOutput);
+
+    if (err < 0) {
+
+        return;
+    }
+
+    OSErr err = MacOpenDriver(serialPortInputName, &serialPortInput);
+
+    if (err < 0) {
+
+        return;
+    }
+
+    setupPBControlForSerialPort(serialPortOutput)
+    setupPBControlForSerialPort(serialPortInput) // TODO: not really sure if this is right for inputs
+
+    outgoingSerialPortReference.ioRefNum = serialPortOutput;
+    incomingSerialPortReference.ioRefNum = serialPortInput; // TODO: not really sure if this is right for inputs
+}
+
+/*
+need code for reading a serial port, maybe this is right
+untested
+
+is PBRead what we want? (From Inside Macintosh Volume II-185)
+PBRead attempts to read ioReqCount bytes from the device driver having the reference number ioRefNum, and transfer them to the data buffer pointed to by ioBuffer.
+ The drive number, if any, of the device to be read from is specified by ioVRefNum. Afterthereadiscompleted,the position is returned in ioPosOffset and the number
+ of bytes actually read is returned in ioActCount.
+*/
+char* readSerialPort() {
+
+    char *stringToReadFromSerial;
+
+    incomingSerialPortReference.ioBuffer = (Ptr) stringToReadFromSerial;
+    incomingSerialPortReference.ioReqCount = 0; // TODO: we need an expected receive length. coprocessor.js needs to supply set chunk sizes?
+    // TODO pretend for now - i'm assuming we need to chunk reads and figure out when to stop in the future
+    // TODO: we need to figure out the proper format for coprocessor, but it needs to be able to tell us that there is more data and that we should call
+    // PBRead repetively.
+
+    OSErr err = PBRead((ParmBlkPtr)& incomingSerialPortReference, 0);
+    // TODO: also need a mechanism for ensuring we are on the right application_id/call_id on the response (thinking of if there are multiple coprocessor.js applications
+    // running on the old Mac at once?)
+    // TODO: we will need to implement a locking mechanism on coprocessor where it must be aware that it can only write one response at a time
+
+    return stringToReadFromSerial;
 }
 
 
-OSErr writeSerialPort(const char* str) { 
+OSErr writeSerialPort(const char* stringToWrite) {
 
-    char str2[10000]; // TODO: why are we creating a new string? to put a linebreak at the end or something? investigate
-    sprintf(str2, "%s\n", str);
+    outgoingSerialPortReference.ioBuffer = (Ptr) stringToWrite;
+    outgoingSerialPortReference.ioReqCount = strlen(stringToWrite);
 
-    pb2.ioBuffer = (Ptr) str2;
-    pb2.ioReqCount = strlen(str2);
-    
-    OSErr err = PBWrite((ParmBlkPtr)& pb2, 0);          
+    // PBWrite Definition From Inside Macintosh Volume II-185:
+    // PBWrite takes ioReqCount bytes from the buffer pointed to by ioBuffer and attempts to write them to the device driver having the reference number ioRefNum.
+    // The drive number, if any, of the device to be written to is specified by ioVRefNum. After the write is completed, the position is returned in ioPosOffset and the number of bytes actually written is returned in ioActCount.
+    OSErr err = PBWrite((ParmBlkPtr)& outgoingSerialPortReference, 0); // second param is async, TODO: investigate
 
     if (err < 0) {
 
-		return err;
-	}
-// TODO: we should only close on program exit, we need to be waiting on reads    
-//    err = MacCloseDriver(serialPort);
-//
-//    return err;
+        return err;
+    }
 }
 
 // TODO: what else does this function need?
-void setupCoprocessor(short refNum) {
-	
-	setupSerialPort(refNum);
-	
-	return;
+// maybe setting application ID in a local state rather than tracking it globally
+void setupCoprocessor(const char *name) {
+
+    setupSerialPort(name);
+
+    return;
 }
 
-// TODO: call on exit
 OSErr closeSerialPort() {
-	
-	OSErr err = MacCloseDriver(pb2.ioRefNum);
+
+    OSErr err = MacCloseDriver(outgoingSerialPortReference.ioRefNum);
 
     return err;
 }
 
 void writeToCoprocessor(char* operation, char* operand) {
-	
-	const char* messageTemplate = "%s;;;%s;;;%s;;;%s"; // see: https://github.com/CamHenlin/coprocessor.js/blob/main/index.js#L25
-	char call_id[32];
-	char messageToSend[10000]; // TODO need to not over/under allocate
-	
-	sprintf(call_id, "%d", call_counter++);
 
-	// application_id is globally defined for now, how will that work in a library?
-	sprintf(messageToSend, messageTemplate, application_id, call_id, operation, operand);
-	
-	OSErr err = writeSerialPort(messageToSend);
+    const char* messageTemplate = "%s;;;%s;;;%s;;;%s"; // see: https://github.com/CamHenlin/coprocessor.js/blob/main/index.js#L25
+    char call_id[32];
+    char messageToSend[10000]; // TODO need to not over/under allocate -- or figure out how to tell coprocessor that we are going to send chunks if we have to
+
+    sprintf(call_id, "%d", call_counter++);
+
+    // application_id is globally defined for now, how will that work in a library?
+    sprintf(messageToSend, messageTemplate, application_id, call_id, operation, operand);
+
+    OSErr err = writeSerialPort(messageToSend);
 }
 
-void sendProgramToCoprocessor(char* program) {
-	
-	writeToCoprocessor("PROGRAM", program);
-	
-	// TODO: handle success after program is loaded on coprocessor
-	
-	return;
+char* sendProgramToCoprocessor(char* program) {
+
+    writeToCoprocessor("PROGRAM", program);
+    char* response = readSerialPort()
+
+    // TODO: rather than just returning the response we should check to see if it was successful then return a boolean
+    return response;
 }
 
-void callFunctionOnCoprocessor(char* functionName, char* parameters) {
-	
-	const char* functionTemplate = "%s&&&%s"; 
-	char functionCallMessage[10000]; // TODO
-	sprintf(functionCallMessage, functionTemplate, functionName, parameters); // TODO: should parameters be a list that we join?
-	
-	writeToCoprocessor("FUNCTION", functionCallMessage);
-	
-	// TODO: get function response
-	
-	return;
+char* callFunctionOnCoprocessor(char* functionName, char* parameters) {
+
+    const char* functionTemplate = "%s&&&%s";
+    char functionCallMessage[10000]; // TODO
+    sprintf(functionCallMessage, functionTemplate, functionName, parameters); // TODO: should parameters be a list that we join?
+
+    writeToCoprocessor("FUNCTION", functionCallMessage);
+    char* response = readSerialPort()
+
+    return response;
 }
 
-void callEvalOnCoprocessor(char* toEval) {
+char* callEvalOnCoprocessor(char* toEval) {
 
-	writeToCoprocessor("EVAL", toEval);
-	
-	// TODO: get eval response
-	
-	return;
+    writeToCoprocessor("EVAL", toEval);
+    char* response = readSerialPort()
+
+    return response;
 }
 
 int main(int argc, char** argv) {
 
-	setupCoprocessor(boutRefNum);
+    setupCoprocessor("printer"); // could also be "modem"
 
-	// TODO: figure out how to move these to Mac resource files. I'm pretty sure the format is pretty easy to follow and extract from
-	const char* TEST_INDEX = "const _ = require('lodash')\nclass SimpleNodeThing {\n  static isEmpty (variable) {\n    return _.isEmpty(variable)\n  }\n  isEmpty (variable) {\n    return this.constructor.isEmpty(variable)\n  }\n}\nmodule.exports = SimpleNodeThing";
-	const char* TEST_PACKAGE = "{\n  \"name\": \"test\",\n  \"version\": \"1.0.0\",\n  \"description\": \"\",\n  \"main\": \"index.js\",\n  \"scripts\": {\n    \"test\": \"\"\n  },\n  \"author\": \"\",\n  \"license\": \"ISC\",\n  \"dependencies\": {\n    \"lodash\": \"^4.17.21\"\n  }\n}";
+    // TODO: figure out how to move these to Mac resource files. I'm pretty sure the format is pretty easy to follow and extract from
+    // started taking a stab at some of this in the Makefile
+    // look at https://github.com/clehner/Browsy/blob/5c3241acd781c0555d02ed329297cb83d66bf9c2/src/uri/about.c#L76 and the corresponding makefile
+    const char* TEST_INDEX = "const _ = require('lodash')\nclass SimpleNodeThing {\n  static isEmpty (variable) {\n    return _.isEmpty(variable)\n  }\n  isEmpty (variable) {\n    return this.constructor.isEmpty(variable)\n  }\n}\nmodule.exports = SimpleNodeThing";
+    const char* TEST_PACKAGE = "{\n  \"name\": \"test\",\n  \"version\": \"1.0.0\",\n  \"description\": \"\",\n  \"main\": \"index.js\",\n  \"scripts\": {\n    \"test\": \"\"\n  },\n  \"author\": \"\",\n  \"license\": \"ISC\",\n  \"dependencies\": {\n    \"lodash\": \"^4.17.21\"\n  }\n}";
 
-	// TODO: set length as required
-	char programCall[10000];
+    // TODO: set length as required
+    char programCall[10000];
 
-	sprintf(programCall, "index.js@@@%s&&&package.json@@@%s", TEST_INDEX, TEST_PACKAGE);
-	
-	sendProgramToCoprocessor(programCall);
-	callFunctionOnCoprocessor("isEmpty", "test");
-	callEvalOnCoprocessor("console.log(this)");
+    sprintf(programCall, "index.js@@@%s&&&package.json@@@%s", TEST_INDEX, TEST_PACKAGE);
 
-	printf("\n(Press Enter to quit)\n");  
-	getchar();
+    sendProgramToCoprocessor(programCall);
+    char* jsFunctionResponse = callFunctionOnCoprocessor("isEmpty", "test");
+    callEvalOnCoprocessor("console.log(this)");
 
-	return 0;
+    printf(jsFunctionResponse);
+    printf("\n(Press Enter to quit)\n");
+    getchar();
+    closeSerialPort();
+
+    return 0;
 }
